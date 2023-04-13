726ba7d475cda113d028f3d11a8f4b2c1d4b0ced
wadashuri <wada20010108@icloud.com>
2023-04-13 21:26:05 +0900

    修正を反映

--- server/resources/views/layouts/admin/app.blade.php
+++ server/resources/views/layouts/admin/app.blade.php
@@ -23,6 +23,18 @@
     <!-- Feather Icons -->
     <script src="https://unpkg.com/feather-icons"></script>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style>
+
+        #new-card, #update-card {
+
+            border: 1px solid #ccc;
+            padding: 8px;
+
+        }
+
+    </style>
+
 </head>
 
 <body>
