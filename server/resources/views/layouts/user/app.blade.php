091a08e666e4ac06c3166eb55d74fca1026c0ea3
shuri <wada20010108@icloud.com>
2023-04-13 15:55:52 +0900

    bg修正

--- server/resources/views/layouts/user/app.blade.php
+++ server/resources/views/layouts/user/app.blade.php
@@ -31,7 +31,7 @@
         <main class="py-4">
             <div class="container-fluid">
                 <div class="row">
-                    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
+                    <nav id="sidebarMenu"  class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
                         @include('layouts.user.sidebar')
                     </nav>
                     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
