40e3b54f5b553d5e6905b683be1ec55967784ecc
wadashuri <wada20010108@icloud.com>
2023-04-24 09:27:24 +0900

    サブスクを作り直したいので一旦登録できないようにする

--- server/resources/views/auth/register.blade.php
+++ server/resources/views/auth/register.blade.php
@@ -63,9 +63,10 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" disabled>
                                     {{ __('Register') }}
                                 </button>
+                                <small class="text-danger">※現在登録することが出来ません</small>
                             </div>
                         </div>
                     </form>
