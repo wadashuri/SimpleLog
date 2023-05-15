81ed7f61f4f194c9e3761203c5e07f3d0e0a9b38
wadashuri <wada20010108@icloud.com>
2023-05-16 01:33:15 +0900

    軽い修正

--- server/resources/views/front/include/work.blade.php
+++ server/resources/views/front/include/work.blade.php
@@ -24,36 +24,36 @@
                   <a href="https://sinceritylab.com/front/post/9" class="img" style="background-image: url(images/work-4.jpg);">
                       <div class="text d-flex justify-content-center align-items-center">
                           <div class="p-3">
-                              <span>音楽</span>
-                              <h3>詳しく見る</h3>
-                          </div>
-                      </div>
-                  </a>
-              </div>
-          </div>
-          <div class="col-md-4 ftco-animate">
-              <div class="work-entry">
-                  <a href="https://sinceritylab.com/front/post/10" class="img" style="background-image: url(images/work-3.jpg);">
-                      <div class="text d-flex justify-content-center align-items-center">
-                          <div class="p-3">
-                              <span>オリジナルサービス「SimpleLog」</span>
-                              <h3>詳しく見る</h3>
-                          </div>
-                      </div>
-                  </a>
-              </div>
-          </div>
-          <div class="col-md-4 ftco-animate">
-              <div class="work-entry">
-                  <a href="https://sinceritylab.com/front/post/11" class="img" style="background-image: url(images/work-1.jpg);">
-                      <div class="text d-flex justify-content-center align-items-center">
-                          <div class="p-3">
                               <span>SES</span>
                               <h3>詳しく見る</h3>
                           </div>
                       </div>
                   </a>
               </div>
+          </div>
+          <div class="col-md-4 ftco-animate">
+            <div class="work-entry">
+                <a href="https://sinceritylab.com/front/post/10" class="img" style="background-image: url(images/work-3.jpg);">
+                    <div class="text d-flex justify-content-center align-items-center">
+                        <div class="p-3">
+                            <span>オリジナルサービス「SimpleLog」</span>
+                            <h3>詳しく見る</h3>
+                        </div>
+                    </div>
+                </a>
+            </div>
+          </div>
+          <div class="col-md-4 ftco-animate">
+            <div class="work-entry">
+                <a href="https://sinceritylab.com/front/post/11" class="img" style="background-image: url(images/work-1.jpg);">
+                    <div class="text d-flex justify-content-center align-items-center">
+                        <div class="p-3">
+                            <span>音楽</span>
+                            <h3>詳しく見る</h3>
+                        </div>
+                    </div>
+                </a>
+            </div>
           </div>
           {{-- <div class="col-md-4 ftco-animate">
               <div class="work-entry">
