9f5c2e58a235687ec08004c70f6b50d636c7ae0d
wadashuri <wada20010108@icloud.com>
2023-05-22 11:58:42 +0900

    その場編集vue3に書き直し

--- server/resources/js/components/editTextCommon/index.vue
+++ server/resources/js/components/editTextCommon/index.vue
@@ -1,53 +1,50 @@
 <!-- その場編集 -->
 <template>
-    <form :action="route" method="POST" ref="editForm"
-        @submit.prevent="handleSubmit">
+    <form :action="route" method="POST" ref="editForm" @submit.prevent="handleSubmit">
         <input type="hidden" name="_method" value="PUT">
-        <input type="text" name="name" class="form-control border-0" v-model="name"
-            @blur="handleBlur">
+        <input type="text" name="name" class="form-control border-0" v-model="name" @blur="handleBlur">
         <input type="hidden" name="_token" :value="csrfToken">
     </form>
 </template>
 
-<script>
-export default {
-    props: {
-        value: {
-            type: Object,
-            required: true,
-        },
-        csrfToken: {
-            type: String,
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        route: {
-            type: String,
-            required: true,
-        },
+<script setup>
+
+import { defineProps, ref } from 'vue';
+
+const props = defineProps({
+    value: {
+        type: Object,
+        required: true,
     },
-    data() {
-        return {
-            name: this.value.name,
-            originalName: this.value.name,
-        };
+    csrfToken: {
+        type: String,
+        required: true,
     },
-    methods: {
-        handleSubmit() {
-            this.$refs.editForm.submit()
-        },
-        handleBlur() {
-            if (this.name !== this.originalName) {
-                if (confirm(`${this.title}名を変更しますか?`)) {
-                    this.handleSubmit();
-                } else {
-                    this.name = this.originalName;
-                }
-            }
-        },
+    title: {
+        type: String,
+        required: true,
     },
-};
+    route: {
+        type: String,
+        required: true,
+    },
+});
+
+const name = ref(props.value.name);
+const originalName = ref(props.value.name);
+const editForm = ref(null);
+
+const handleSubmit = () => {
+    editForm.value.submit()
+}
+
+const handleBlur = () => {
+    if (name.value !== originalName.value) {
+        if (confirm(`${props.title}名を変更しますか?`)) {
+            handleSubmit();
+        } else {
+            name.value = originalName.value;
+        }
+    }
+}
 </script>
